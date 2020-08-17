33435c4acdca2bb1080af23fae7252fcde55e66b
Boykarne <61529062+Boykarne@users.noreply.github.com>
2020-08-17 21:45:59 +0800

    Add files via upload
    pre ito

---
+++ WindowsFormsApp8/WindowsFormsApp8/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
